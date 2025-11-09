b65323a3d848bf5ff434412a03870ca3daf261c1
PhuongY <phambuinamphuong@gmail.com>
2025-11-10 00:23:04 +0700

    Update action flows - Updated CI-CD flow - Updated status monitor - Added APK for android build - Added secrets leaks

--- BE/scripts/init-databases.sql
+++ BE/scripts/init-databases.sql
@@ -1,11 +1,19 @@
 -- Initialize databases for Delivery System Services
 CREATE DATABASE IF NOT EXISTS ds_user_service;
 CREATE DATABASE IF NOT EXISTS ds_settings_service;
+CREATE DATABASE IF NOT EXISTS ds_zone_service;
+CREATE DATABASE IF NOT EXISTS ds_parcel_service;
+CREATE DATABASE IF NOT EXISTS ds_session_service;
+CREATE DATABASE IF NOT EXISTS ds_communication_service;
 
 -- Create user for services (optional, can use root for development)
 -- CREATE USER 'dss_user'@'%' IDENTIFIED BY 'dss_password';
 -- GRANT ALL PRIVILEGES ON ds_user_service.* TO 'dss_user'@'%';
 -- GRANT ALL PRIVILEGES ON ds_settings_service.* TO 'dss_user'@'%';
+-- GRANT ALL PRIVILEGES ON ds_zone_service.* TO 'dss_user'@'%';
+-- GRANT ALL PRIVILEGES ON ds_parcel_service.* TO 'dss_user'@'%';
+-- GRANT ALL PRIVILEGES ON ds_session_service.* TO 'dss_user'@'%';
+-- GRANT ALL PRIVILEGES ON ds_communication_service.* TO 'dss_user'@'%';
 -- GRANT ALL PRIVILEGES ON ds_order_service.* TO 'dss_user'@'%';
 -- GRANT ALL PRIVILEGES ON ds_delivery_service.* TO 'dss_user'@'%';
 -- FLUSH PRIVILEGES;
